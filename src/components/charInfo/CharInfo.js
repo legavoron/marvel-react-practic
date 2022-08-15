3ade5b30f0f6744c66c2f5a74ad3ff03db95ac58
Aleh Shchurko <alehshchurko@gmail.com>
2022-08-15 20:55:35 +0300

    make it better

--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -1,4 +1,5 @@
-import { Component } from 'react/cjs/react.production.min';
+import { Component } from 'react';
+import PropTypes from 'prop-types';
 
 import MarvelService from '../../services/MarvelService';
 import Spinner from '../spinner/spinner';
@@ -125,6 +126,10 @@ const View = ({char}) => {
             </ul>
         </>
     )
+}
+
+CharInfo.propTypes = {
+    charId: PropTypes.number
 }
 
 export default CharInfo;
